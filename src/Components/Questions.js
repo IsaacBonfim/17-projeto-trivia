453b060665c11ea6105b4eaee329338cc6ab01c9
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-04-06 18:59:31 -0300

    tentativa de soluçao da 7

--- src/Components/Questions.js
+++ src/Components/Questions.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { getQuestions } from '../Action';
 // import Question from './Question';
 
 class Questions extends React.Component {
