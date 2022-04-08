467772efd0e83e51e6ebba7696924c4547792dbd
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-04-08 16:46:27 -0300

    Requisito 18

--- src/Pages/Ranking.js
+++ src/Pages/Ranking.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import getRanking from '../Helpers';
 
 class Ranking extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      ranking: [],
+    };
+  }
+
+  componentDidMount() {
+    const ranking = getRanking();
+    ranking.sort((a, b) => b.score - a.score);
+    this.setState({ ranking });
+  }
+
   render() {
     const { history } = this.props;
+    const { ranking } = this.state;
     return (
       <section>
-        <h1 data-testid="ranking-title">Requisito16</h1>
+        <h1 data-testid="ranking-title">Ranking:</h1>
+
+        <ol>
+          { ranking.map(({ name, score, picture }, index) => (
+            <li key={ index }>
+              <p data-testid={ `player-name-${index}` }>{ name }</p>
+              <p data-testid={ `player-score-${index}` }>{ score }</p>
+              <img src={ picture } alt={ name } />
+            </li>
+          )) }
+        </ol>
         <button
           type="button"
           data-testid="btn-go-home"
